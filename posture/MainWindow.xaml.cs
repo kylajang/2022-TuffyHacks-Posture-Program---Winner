75477fc5b126e8198a89bd399e7155c755e7ed68
Kyla Jang <kylajang@csu.fullerton.edu>
2022-02-26 05:36:19 -0800

    created GUI checkboxes and start function

--- posture/MainWindow.xaml.cs
+++ posture/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace posture
 {
@@ -25,10 +26,24 @@ namespace posture
         SerialPort sensorPort = new SerialPort();
 
         string serialData;
+        public int RightThigh { get; set; }
+        public int LeftThigh { get; set; }
+        public int RightButt { get; set; }
+        public int LeftButt { get; set; }
+        // Parsing serial data into 4 integer variables
+        public void SerialData(string[] parsedInfo)
+        {
+            RightThigh = Convert.ToInt32(parsedInfo[0]);
+            LeftThigh = Convert.ToInt32(parsedInfo[1]);
+            RightButt = Convert.ToInt32(parsedInfo[2]);
+            LeftButt = Convert.ToInt32(parsedInfo[3]);
+        }
 
         public MainWindow()
         {
             InitializeComponent();
+
+
 
             try
             {
@@ -57,12 +72,49 @@ namespace posture
 
                 this.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
                 {
-                    sensorOutput.Content = "Sensor Data: " + serialData;
+                    
+
                 }));
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (((bool)goodposture.IsChecked && !((bool)morethanthree.IsChecked)))
+            {
+                string file = @"C:\Users\ky\source\repos\posture\SurveyInput.txt";
+                string text = "This user believes they have good posture and they sit for less than 3 hours";
+                File.WriteAllText(file, text);
+                Console.WriteLine(File.ReadAllText(file));
+                Console.WriteLine();
+            }
+            else if (((bool)goodposture.IsChecked && !((bool)lessthanthree.IsChecked)))
+            {
+                string file = @"C:\Users\ky\source\repos\posture\SurveyInput.txt";
+                string text = "This user believes they have good posture and they sit for more than 3 hours.";
+                File.WriteAllText(file, text);
+                Console.WriteLine(File.ReadAllText(file));
+                Console.WriteLine();
+            }
+            else if (((bool)badposture.IsChecked && !((bool)lessthanthree.IsChecked)))
+            {
+                string file = @"C:\Users\ky\source\repos\posture\SurveyInput.txt";
+                string text = "This user believes they have bad posture and they sit for less than 3 hours.";
+                File.WriteAllText(file, text);
+                Console.WriteLine(File.ReadAllText(file));
+                Console.WriteLine();
+            }
+            else if (((bool)badposture.IsChecked && !((bool)morethanthree.IsChecked)))
+            {
+                string file = @"C:\Users\ky\source\repos\posture\SurveyInput.txt";
+                string text = "This user believes they have bad posture and they sit for more than 3 hours.";
+                File.WriteAllText(file, text);
+                Console.WriteLine(File.ReadAllText(file));
+                Console.WriteLine();
             }
         }
     }
