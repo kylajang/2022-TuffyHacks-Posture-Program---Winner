690ed99a510ea1baa621ba6bf5b593c9a932526f
Timothy Kheang <tim.kheang@hotmail.com>
2022-02-26 01:16:16 -0800

    Added serial setup code

--- posture/MainWindow.xaml.cs
+++ posture/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Ports;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,43 @@ namespace posture
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort sensorPort = new SerialPort();
+
+        string serialData;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            try
+            {
+                sensorPort.PortName = "COM4";
+                sensorPort.BaudRate = 115200;
+                sensorPort.Parity = Parity.None;
+                sensorPort.StopBits = StopBits.One;
+                sensorPort.Handshake = Handshake.None;
+
+                sensorPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            sensorPort.DataReceived += new SerialDataReceivedEventHandler(sensorPort_DataReceived);
+        }
+
+        private void sensorPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                serialData = sensorPort.ReadLine();
+                Console.WriteLine(serialData);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
