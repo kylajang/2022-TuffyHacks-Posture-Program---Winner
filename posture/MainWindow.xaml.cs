06cf6fbf17b63413a5627c8a1abdc251f15ee469
Timothy Kheang <tim.kheang@hotmail.com>
2022-02-26 07:08:39 -0800

    Final version for project submission

--- posture/MainWindow.xaml.cs
+++ posture/MainWindow.xaml.cs
@@ -23,9 +23,20 @@ namespace posture
     /// </summary>
     public partial class MainWindow : Window
     {
+        // SerialPort object for collecting sensor data
         SerialPort sensorPort = new SerialPort();
 
+        // Timer for measuring how long the user has been sitting down
+        DispatcherTimer sitDownTimer = new DispatcherTimer();
+
+        // Sensor data variables
         string serialData;
+        bool readingSerialData = false;
+
+        // Sitting down timer variables
+        int timeSatDown;
+        int maxSitDownTime = 3600;
+
         public int RightThigh { get; set; }
         public int LeftThigh { get; set; }
         public int RightButt { get; set; }
@@ -33,10 +44,17 @@ namespace posture
         // Parsing serial data into 4 integer variables
         public void SerialData(string[] parsedInfo)
         {
-            RightThigh = Convert.ToInt32(parsedInfo[0]);
-            LeftThigh = Convert.ToInt32(parsedInfo[1]);
-            RightButt = Convert.ToInt32(parsedInfo[2]);
-            LeftButt = Convert.ToInt32(parsedInfo[3]);
+            try
+            {
+                LeftThigh = Convert.ToInt32(parsedInfo[0]);
+                RightThigh = Convert.ToInt32(parsedInfo[1]);
+                LeftButt = Convert.ToInt32(parsedInfo[2]);
+                RightButt = Convert.ToInt32(parsedInfo[3]);
+            }
+            catch
+            {
+
+            }
         }
 
         public MainWindow()
@@ -52,15 +70,37 @@ namespace posture
                 sensorPort.Parity = Parity.None;
                 sensorPort.StopBits = StopBits.One;
                 sensorPort.Handshake = Handshake.None;
-
-                sensorPort.Open();
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                // MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
             sensorPort.DataReceived += new SerialDataReceivedEventHandler(sensorPort_DataReceived);
+
+            // Initialize serial port received data handler
+            sensorPort.DataReceived += new SerialDataReceivedEventHandler(sensorPort_DataReceived);
+
+            // Initialize sitting down timer
+            // Call SitDownTimer_Tick every second
+            sitDownTimer.Tick += SitDownTimer_Tick;
+            sitDownTimer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        // Function to update the total time the user has been sitting down
+        private void SitDownTimer_Tick(object sender, EventArgs e)
+        {
+            timeSatDown++;
+            // If the user has been sitting down for one hour, show a break reminder message
+            if (timeSatDown > maxSitDownTime)
+            {
+                while (MessageBox.Show("You've been sitting down for one hour! Please take a few minutes to walk and stretch.", "Break Time!", MessageBoxButton.OK, MessageBoxImage.Information) != MessageBoxResult.OK)
+                {
+
+                }
+                // Reset the total time sat down 
+                timeSatDown = 0;
+            }
         }
 
         private void sensorPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -70,10 +110,14 @@ namespace posture
                 serialData = sensorPort.ReadLine();
                 Console.WriteLine(serialData);
 
+                string[] serialDataArray = serialData.Split(',');
+
+                SerialData(serialDataArray);
+
                 this.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
                 {
                     //connected to label and tells user to lean left, right, or forward based off right thigh input
-                    if (RightThigh < 945 && LeftThigh < 945)
+                    if (RightButt > 999 && LeftButt > 999)
                     {
                         rightthigh.Content = "Right Thigh Output: " + RightThigh.ToString();
                         userdirection.Content = "\nlean forward";
@@ -94,7 +138,7 @@ namespace posture
                         userdirection.Content = "\ngood posture";
                     }
                     //connected to label and tells user to lean left, right, or forward based off left thigh input
-                    if (RightThigh < 945 && LeftThigh < 945)
+                    if (RightButt > 999 && LeftButt > 999)
                     {
                         leftthigh.Content = "Left Thigh Output: " + LeftThigh.ToString();
                         userdirection.Content = "\nlean forward";
@@ -161,7 +205,7 @@ namespace posture
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                // MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
@@ -169,7 +213,7 @@ namespace posture
         {
             if (((bool)goodposture.IsChecked && !((bool)morethanthree.IsChecked)))
             {
-                string file = @"C:\Users\casti\source\repos\posture\SurveyInput.txt";
+                string file = @"C:\Users\timkh\source\repos\posture\SurveyInput.txt";
                 string text = "This user believes they have good posture and they sit for less than 3 hours";
                 File.WriteAllText(file, text);
                 Console.WriteLine(File.ReadAllText(file));
@@ -177,7 +221,7 @@ namespace posture
             }
             else if (((bool)goodposture.IsChecked && !((bool)lessthanthree.IsChecked)))
             {
-                string file = @"C:\Users\casti\source\repos\posture\SurveyInput.txt";
+                string file = @"C:\Users\timkh\source\repos\posture\SurveyInput.txt";
                 string text = "This user believes they have good posture and they sit for more than 3 hours.";
                 File.WriteAllText(file, text);
                 Console.WriteLine(File.ReadAllText(file));
@@ -185,7 +229,7 @@ namespace posture
             }
             else if (((bool)badposture.IsChecked && !((bool)lessthanthree.IsChecked)))
             {
-                string file = @"C:\Users\casti\source\repos\posture\SurveyInput.txt";
+                string file = @"C:\Users\timkh\source\repos\posture\SurveyInput.txt";
                 string text = "This user believes they have bad posture and they sit for less than 3 hours.";
                 File.WriteAllText(file, text);
                 Console.WriteLine(File.ReadAllText(file));
@@ -193,11 +237,27 @@ namespace posture
             }
             else if (((bool)badposture.IsChecked && !((bool)morethanthree.IsChecked)))
             {
-                string file = @"C:\Users\casti\source\repos\posture\SurveyInput.txt";
+                string file = @"C:\Users\tkheang\source\repos\posture\SurveyInput.txt";
                 string text = "This user believes they have bad posture and they sit for more than 3 hours.";
                 File.WriteAllText(file, text);
                 Console.WriteLine(File.ReadAllText(file));
                 Console.WriteLine();
+            }
+
+            if (readingSerialData == false && sensorPort.IsOpen == false)
+            {
+                readingSerialData = true;
+                startStopButton.Content = "Stop";
+                sensorPort.Open();
+                sitDownTimer.Start();
+            }
+            else
+            {
+                readingSerialData = false;
+                startStopButton.Content = "Start";
+                sensorPort.Close();
+                sitDownTimer.Stop();
+                timeSatDown = 0;
             }
         }
     }
